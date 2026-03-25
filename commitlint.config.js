a358d70e01d2dfbb10bc4109f5b87dba64d55b82
thecodingmontana <dev.humblechris254@gmail.com>
2026-03-25 17:22:13 +0300

    chore(app): :hammer: migrate from standard-version to release-it
    - replace standard-version with release-it and @release-it/conventional-changelog
    
    - add .release-it.json with conventional commits preset
    
    - update release scripts in package.json
    
    - fix auto version bump not respecting commit types

--- commitlint.config.js
+++ commitlint.config.js
@@ -43,23 +43,28 @@ module.exports = {
       {
         value: "feat",
         name: "feat:     ✨  A new feature",
+        emoji: ":sparkles:",
       },
-      { value: "fix", name: "fix:      🐛  A bug fix" },
+      { value: "fix", name: "fix:      🐛  A bug fix", emoji: ":bug:" },
       {
         value: "docs",
         name: "docs:     📝  Documentation only changes",
+        emoji: ":memo:",
       },
       {
         value: "style",
         name: "style:    💄  Changes that do not affect the meaning of the code",
+        emoji: ":lipstick:",
       },
       {
         value: "refactor",
         name: "refactor: ♻️   A code change that neither fixes a bug nor adds a feature",
+        emoji: ":recycle:",
       },
       {
         value: "perf",
         name: "perf:     ⚡️  A code change that improves performance",
+        emoji: ":zap:",
       },
       {
         value: "test",
@@ -69,22 +74,26 @@ module.exports = {
       {
         value: "build",
         name: "build:    📦️  Changes that affect the build system or external dependencies",
+        emoji: ":package:",
       },
       {
         value: "ci",
         name: "ci:       🎡  Changes to our CI configuration files and scripts",
+        emoji: ":ferris_wheel:",
       },
       {
         value: "chore",
         name: "chore:    🔨  Other changes that don't modify src or test files",
+        emoji: ":hammer:",
       },
       {
         value: "revert",
         name: "revert:   ⏪️  Reverts a previous commit",
+        emoji: ":rewind:",
       },
-      { value: "init", name: "init:     🎉  First commit" },
+      { value: "init", name: "init:     🎉  First commit", emoji: ":tada:" },
     ],
-    useEmoji: false,
+    useEmoji: true,
     emojiAlign: "center",
     useAI: false,
     aiNumber: 1,
