1e163577a1f19f2ef1612f0c23ac5b90b22053f4
thecodingmontana <dev.humblechris254@gmail.com>
2026-03-25 16:58:33 +0300

    fix(app): removed use-emoji in commitlint to fix standard version bumping versioning

--- commitlint.config.js
+++ commitlint.config.js
@@ -43,28 +43,23 @@ module.exports = {
       {
         value: "feat",
         name: "feat:     ✨  A new feature",
-        emoji: ":sparkles:",
       },
-      { value: "fix", name: "fix:      🐛  A bug fix", emoji: ":bug:" },
+      { value: "fix", name: "fix:      🐛  A bug fix" },
       {
         value: "docs",
         name: "docs:     📝  Documentation only changes",
-        emoji: ":memo:",
       },
       {
         value: "style",
         name: "style:    💄  Changes that do not affect the meaning of the code",
-        emoji: ":lipstick:",
       },
       {
         value: "refactor",
         name: "refactor: ♻️   A code change that neither fixes a bug nor adds a feature",
-        emoji: ":recycle:",
       },
       {
         value: "perf",
         name: "perf:     ⚡️  A code change that improves performance",
-        emoji: ":zap:",
       },
       {
         value: "test",
@@ -74,26 +69,22 @@ module.exports = {
       {
         value: "build",
         name: "build:    📦️  Changes that affect the build system or external dependencies",
-        emoji: ":package:",
       },
       {
         value: "ci",
         name: "ci:       🎡  Changes to our CI configuration files and scripts",
-        emoji: ":ferris_wheel:",
       },
       {
         value: "chore",
         name: "chore:    🔨  Other changes that don't modify src or test files",
-        emoji: ":hammer:",
       },
       {
         value: "revert",
         name: "revert:   ⏪️  Reverts a previous commit",
-        emoji: ":rewind:",
       },
-      { value: "init", name: "init:     🎉  First commit", emoji: ":tada:" },
+      { value: "init", name: "init:     🎉  First commit" },
     ],
-    useEmoji: true,
+    useEmoji: false,
     emojiAlign: "center",
     useAI: false,
     aiNumber: 1,
